8380b98a647cd7252234dfbba1ada41a65ead87c
Davi Gabriel <domingues.gabriel.davi@gmail.com>
2025-12-06 23:55:09 -0300

    Edição singela da main.

--- codigosPython/main.py
+++ codigosPython/main.py
@@ -19,22 +19,27 @@ if __name__ == "__main__":
 
     # 2. Loop de Aplicação
     while True:
-        # Exibe o menu e captura a escolha
-        opcao = view.exibir_menu_principal()
+        try:
+            # Exibe o menu e captura a escolha
+            opcao = view.exibir_menu_principal()
 
-        if opcao == "1":
-            # Equivalente ao Cenário 1 e 2
-            # A própria View vai pedir os dados (Título, Data, etc.) e tratar tanto o sucesso (201) quanto o erro de disciplina (404)
-            view.renderizar_criar_tarefa()
+            if opcao == "1":
+                # Equivalente ao Cenário 1 e 2
+                # A própria View vai pedir os dados (Título, Data, etc.) e tratar tanto o sucesso (201) quanto o erro de disciplina (404)
+                view.renderizar_criar_tarefa()
 
-        elif opcao == "2":
-            # Equivalente ao Cenário 3
-            # A View pede o ID e chama o controller para concluir
-            view.renderizar_concluir_tarefa()
+            elif opcao == "2":
+                # Equivalente ao Cenário 3
+                # A View pede o ID e chama o controller para concluir
+                view.renderizar_concluir_tarefa()
 
-        elif opcao == "0":
-            print("Saindo do sistema...")
+            elif opcao == "0":
+                print("Saindo do sistema...")
+                break
+
+            else:
+                print("Opção inválida, tente novamente.")
+
+        except KeyboardInterrupt:
+            print("\nSaindo do sistema...")
             break
-
-        else:
-            print("Opção inválida, tente novamente.")
